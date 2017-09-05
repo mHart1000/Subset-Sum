fef6e469d65a0e3e978df177db1a952b1fbb13cf
Matthew Hart <matthewhart1000@gmail.com>
2017-09-05 07:58:01 -0500

    added error-handling & formatted output

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+Script written in Python
+
+Should work on any machine running Python 2.7+
+
+Clone Github repo from:
+https://github.com/mHart1000/Subset-Sum.git
+
+From windows or linux command line, enter the path to the python script followed by ">" followed by the path to the data file
+Example in Windows:
+c:\users\matthew\desktop\subset-sum\app.py < c:\users\matthew\desktop\data.csv
+
+
--- app.py
+++ app.py
@@ -14,13 +14,14 @@ try:
 	food_list = [line[0].strip() for line in data][1:]
 	prices = [float(line[1].strip()[1:]) for line in data][1:]
 except Exception:
-	print("Problem formatting variables, please make sure input points to a valid csv file")
+	print("Formatting error, please ensure input points to a valid csv file")
 
 	
 # Look for any combinations of numbers from a list that add up to a target
 def subset_sum(numbers, target, menu_items,	partial=[]):
 	s = sum(partial)
 	
+	 # If solution is found, return formatted items & prices
 	if s == target:
 		answer = dict([[menu_items[prices.index(price)], 
 		"${:,.2f}".format(price)] for price in partial])
@@ -30,17 +31,17 @@ def subset_sum(numbers, target, menu_items,	partial=[]):
 		
 		return True # Pass flag
 	if s > target:
-		return  
+		return None
 
 	for i in range(len(numbers)):
 		n = numbers[i]
 		remaining = numbers[i+1:]
-		if subset_sum(remaining, target, menu_items, partial + [n]) == True: # Pass flag
+		if subset_sum(remaining, target, menu_items, partial + [n]):
 			return True; 
 
-			
+# Check flag for negative output while running function			
 try:			
-	if subset_sum(prices, target_value, food_list) is not True: # Check flag while running function
+	if not subset_sum(prices, target_value, food_list): 
 		print("No solution exists");
 except Exception as e:
 	print(e)	
